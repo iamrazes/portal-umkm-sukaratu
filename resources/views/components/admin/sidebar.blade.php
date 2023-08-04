906d4ceed51fc5f520b34b5157d554c327da279e
RAZES <71701718+iamrazes@users.noreply.github.com>
2023-08-04 13:21:26 +0700

    Update sidebar v0
    initial / starting point for sidebar

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,37 +1,71 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+      <!-- Brand Logo -->
+      <a href="{{ route('dashboard') }}" class="brand-link">
+          <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+              class="brand-image img-circle elevation-3" style="opacity: .8">
+          <span class="brand-text font-weight-light">Admin Dashboard</span>
+      </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
+      <!-- Sidebar -->
+      <div class="sidebar">
+          <!-- Sidebar user panel (optional) -->
+          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+              <div class="image">
+                  <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                      alt="User Image">
+              </div>
+              <div class="info">
+                  <a href="{{ route('profile.edit') }}" class="d-block">{{ Auth::user()->name }}</a>
+              </div>
           </div>
-        </div>
-      </div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
+          <!-- SidebarSearch Form -->
+          <div class="form-inline">
+              <div class="input-group" data-widget="sidebar-search">
+                  <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                      aria-label="Search">
+                  <div class="input-group-append">
+                      <button class="btn btn-sidebar">
+                          <i class="fas fa-search fa-fw"></i>
+                      </button>
+                  </div>
+              </div>
+          </div>
+          <nav class="mt-2">
+              <ul class="nav nav-pills nav-sidebar flex-column">
+                  <li class="nav-item">
+                      <a href="{{route('dashboard')}}" class="nav-link active">
+                          <i class="nav-icon fa fa-th-list"></i>
+                          <p>Dashboard</p>
+                      </a>
+                  </li>
+
+                  <li class="nav-header">CONTENT CONTROL</li>
+                  <li class="nav-item">
+                      <a href="" class="nav-link ">
+                          <i class="nav-icon fas fa-shopping-cart"></i>
+                          <p>Products</p>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="" class="nav-link ">
+                          <i class="nav-icon far fa-image"></i>
+                          <p>Galleries</p>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="" class="nav-link ">
+                          <i class="nav-icon fa fa-feather"></i>
+                          <p>Stories</p>
+                      </a>
+                  </li>
+                  <li class="nav-header">SYSTEM CONTROL</li>
+                  <li class="nav-header">MISCELLANEOUS</li>
+              </ul>
+          </nav>
+          <!-- Sidebar Menu -->
+          {{-- <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
@@ -667,8 +701,8 @@
             </a>
           </li>
         </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+      </nav> --}}
+          <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
   </aside>
