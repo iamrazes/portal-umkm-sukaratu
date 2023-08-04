4e6e5126f62d8c3c0818ff04cc40acde40209b3e
RAZES <71701718+iamrazes@users.noreply.github.com>
2023-08-04 10:31:17 +0700

    Migration minor fixes
    added image string
    product's models set

--- database/migrations/2023_08_03_145842_create_products_table.php
+++ database/migrations/2023_08_03_145842_create_products_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100)->nullable()->default('text');
-            $table->longText('description')->nullable()->default('description');
+            $table->string('name', 100)->nullable();
+            $table->string('image')->nullable();
+            $table->longText('description')->nullable();
             $table->double('price', 15, 8)->nullable()->default(123.4567);
             $table->timestamps();
         });
