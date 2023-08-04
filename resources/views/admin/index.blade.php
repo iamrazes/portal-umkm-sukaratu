2bf1f1887d8d7d24ac69f45bac14f61aa88c2db3
RAZES <71701718+iamrazes@users.noreply.github.com>
2023-08-04 12:32:44 +0700

    Implemented AdminLTE for Dashboard
    layouting

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+@section('header')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ route('dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+@endsection
+
+@section('content')
+<h1></h1>
+@endsection
